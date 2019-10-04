e4bfac74ea5d4927d2ff24ce96ed0f496513a4bc
Arthur Moreno <morenoarthur52@gmail.com>
2019-10-04 00:26:20 -0300

    User entity and resource

---
+++ src/main/java/com/morenoweb/course/resources/UserResource.java
@@ -0,0 +1,27 @@
+package com.morenoweb.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.morenoweb.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findall(){
+		
+		User u = new User(1L, "Arthur Moreno", "morenoarthur52@gmail.com", "31012341", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+	
+	
+	
+	
+	
+	
+
+}
